9fdef84744c13704f85544c2666a188d21440990
Dmitry <hodas.work@gmail.com>
2023-05-01 19:03:29 +0300

    add form create article

--- blog/forms/article.py
+++ blog/forms/article.py
@@ -1,9 +1,10 @@
 import wtforms
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, SubmitField, TextAreaField
+from wtforms import StringField, validators, SubmitField, TextAreaField, SelectMultipleField
 
 
 class ArticleAddForm(FlaskForm):
     title = StringField('Title', [validators.DataRequired()])
     text = TextAreaField('Text', [validators.DataRequired()])
+    tags = SelectMultipleField('Tags', coerce=int)
     submit = SubmitField('Add')
