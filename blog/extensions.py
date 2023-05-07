7bc99741f3eb7cba1102aa6ce14b440b739e3a3b
Dmitry <hodas.work@gmail.com>
2023-05-07 21:30:26 +0300

    add documentations

--- blog/extensions.py
+++ blog/extensions.py
@@ -6,11 +6,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
 
+from blog.admin.views import CustomAdminIndexView
+
 db = SQLAlchemy()
 login_manager = LoginManager()
 migrate = Migrate()
 csrf = CSRFProtect()
-admin = Admin(name='Admin panel', template_mode='bootstrap4')
+admin = Admin(index_view=CustomAdminIndexView(), name='Admin panel', template_mode='bootstrap4')
 api = Api()
 
 
